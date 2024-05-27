515402222cb5e2bdc557f78d46bf443236a9ec62
karthik Sudhan <karthiksudhan.sg2021@vitstudent.ac.in>
2024-05-27 22:39:58 +0530

    nextgreater

--- SingleNumber.java
+++ SingleNumber.java
@@ -40,7 +40,7 @@ class SingleNumber {
     }
 
     public static void main(String[] args){
-        int nums[]={4,1,2,1,2};
+        int nums[]={4,2,1,1,2};
         int out=s_Number(nums);
         System.out.println(out);
 
