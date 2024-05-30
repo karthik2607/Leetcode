86f5d8890b9a05829b73447cdec9aa816aad48fb
karthik Sudhan <karthiksudhan.sg2021@vitstudent.ac.in>
2024-05-30 14:17:30 +0530

    duplicate

---
+++ FindDuplicate.java
@@ -0,0 +1,66 @@
+//287
+//https://leetcode.com/problems/find-the-duplicate-number/description/
+//30.5.24 -revisited
+//There is code for inplace sort to maintain o(1) space complexity
+//check out
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+// Example 2:
+
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+// Example 3:
+
+// Input: nums = [3,3,3,3,3]
+// Output: 3
+ 
+
+// Constraints:
+
+// 1 <= n <= 105
+// nums.length == n + 1
+// 1 <= nums[i] <= n
+// All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+// Follow up:
+
+// How can we prove that at least one duplicate number must exist in nums?
+// Can you solve the problem in linear runtime complexity?
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                int temp=nums[i];
+                nums[i]=nums[correct];
+                nums[correct]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                return nums[j];
+            }
+        }
+        return nums.length;
+
+        
+    }
+}
