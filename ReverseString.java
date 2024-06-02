4c432285196f0c3a78e0f886873b91480cb28144
karthik Sudhan <karthiksudhan.sg2021@vitstudent.ac.in>
2024-06-02 10:51:39 +0530

    reverse string daily problem

---
+++ ReverseString.java
@@ -0,0 +1,39 @@
+//344
+//2.6.24
+//https://leetcode.com/problems/reverse-string/description/?envType=daily-question&envId=2024-06-02
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+        
+    }
+}
