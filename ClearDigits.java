ee90bf3cbbc19dc58355768846a8e40ff94c6a0c
karthik Sudhan <karthiksudhan.sg2021@vitstudent.ac.in>
2024-06-12 14:27:06 +0530

    clear Digits stringbuilder

---
+++ ClearDigits.java
@@ -0,0 +1,60 @@
+//3174
+//12.6.24
+//String builder
+//https://leetcode.com/problems/clear-digits/submissions/1285791123/
+
+// You are given a string s.
+
+// Your task is to remove all digits by doing this operation repeatedly:
+
+// Delete the first digit and the closest non-digit character to its left.
+// Return the resulting string after removing all digits.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc"
+
+// Output: "abc"
+
+// Explanation:
+
+// There is no digit in the string.
+
+// Example 2:
+
+// Input: s = "cb34"
+
+// Output: ""
+
+// Explanation:
+
+// First, we apply the operation on s[2], and s becomes "c4".
+
+// Then we apply the operation on s[1], and s becomes "".
+
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists only of lowercase English letters and digits.
+// The input is generated such that it is possible to delete all digits.
+
+
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                sb.deleteCharAt(sb.length()-1);
+                
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+        
+    }
+}
